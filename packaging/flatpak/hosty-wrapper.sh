eb81f6e39abca80fc6e7c9c178b1c0322ad8b2a5
Sugarycandybar <candybaralt@gmail.com>
2026-05-01 20:49:22 -0400

    unneccesary comments

--- packaging/flatpak/hosty-wrapper.sh
+++ packaging/flatpak/hosty-wrapper.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 set -eu
-# Ensure Hosty stores data under the sandboxed XDG data directory so
-# the app does not need broad access to the user's $HOME.
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 HOSTY_DATA_DIR="${HOSTY_DATA_DIR:-$XDG_DATA_HOME/hosty}"
 export HOSTY_DATA_DIR
